0f166c46f7b711bf67c4929693f9251ad9379fdc
Pallavi N Kini <pallavikini5566@gmail.com>
2024-04-19 02:39:19 +0530

    added css

--- form.js
+++ form.js
@@ -3,8 +3,9 @@ import data from "./sampleData.json" assert { type: "json" };
 const formData = data.formData;
 // console.log(formData);
 var newArray = [];
+let finalArray = [];
 newArray = formData;
-
+let count = 1;
 const getJsonData = () => {
   const contentDiv = document.getElementById("formDesigner");
 
@@ -22,14 +23,22 @@ const getJsonData = () => {
     const contentdiv = document.createElement("div");
     contentdiv.textContent = dataItem.type;
     div.appendChild(contentdiv);
-
+    count++;
+    finalArray.push(dataItem);
     // Create a span for the plus icon
     const plusIcon = document.createElement("span");
     plusIcon.innerHTML = '<i class="fa-solid fa-plus"></i>';
     plusIcon.classList.add("plus-icon");
     div.appendChild(plusIcon);
     plusIcon.addEventListener("click", () => {
-      handlePlusIconClick(dataItem);
+      const oject = {};
+      oject.id = finalArray.length + 1;
+      oject.label = dataItem.label;
+      oject.type = dataItem.type;
+      oject.placeholder = dataItem.placeholder;
+      oject.options = dataItem.options;
+      finalArray.push(oject);
+      handlePlusIconClick(oject);
     });
 
     // Append the main container div to the content div
@@ -44,14 +53,81 @@ const handlePlusIconClick = (dataItem) => {
   createForm();
 };
 
+const updateNewArrayOrder = () => {
+  const updatedArray = [];
+  formContainer.childNodes.forEach((node) => {
+    const eles = finalArray.filter(
+      (ele) => ele.id === +node.id.charAt(node.id.length - 1)
+    );
+    if (eles && eles.length > 0) {
+      updatedArray.push(eles[0]);
+    }
+  });
+  newArray = updatedArray;
+  finalArray = updatedArray;
+};
+
 const createForm = () => {
-  console.log(newArray);
   const formContainer = document.getElementById("formContainer");
 
   //Create outer container for each form field
   newArray.forEach((item) => {
     const outerDiv = document.createElement("div");
     outerDiv.classList.add("form-field-container");
+    outerDiv.setAttribute("draggable", "true");
+    outerDiv.setAttribute("id", "main" + item.id);
+
+    outerDiv.addEventListener("dragstart", (event) => {
+      event.dataTransfer.setData("text/plain", "main" + item.id);
+    });
+
+    outerDiv.addEventListener("dragover", (e) => {
+      e.preventDefault(); // Allow drop
+    });
+
+    outerDiv.addEventListener("drop", (e) => {
+      e.preventDefault();
+      const startIndex = e.dataTransfer.getData("text/plain");
+      const endIndex = e.currentTarget.id;
+      if (startIndex !== endIndex) {
+        // Reorder the elements in the DOM
+        const draggedElement = document.getElementById(startIndex);
+        const dropZoneElement = document.getElementById(endIndex);
+
+        // Get the index of the drop zone element in the list of form fields
+        const dropZoneIndex = Array.from(formContainer.children).indexOf(
+          dropZoneElement
+        );
+
+        // Check if the dragged element should be inserted before or after the drop zone element
+        if (draggedElement.offsetTop > dropZoneElement.offsetTop) {
+          // Dragging upwards
+          formContainer.insertBefore(draggedElement, dropZoneElement);
+        } else if (draggedElement.offsetTop < dropZoneElement.offsetTop) {
+          // Dragging downwards
+          if (formContainer.children[dropZoneIndex + 1]) {
+            // If there is a next sibling, insert before it
+            formContainer.insertBefore(
+              draggedElement,
+              formContainer.children[dropZoneIndex + 1]
+            );
+          } else {
+            // If there is no next sibling, append to the end
+            formContainer.appendChild(draggedElement);
+          }
+        }
+        // Update the newArray with the new order
+        updateNewArrayOrder();
+      }
+    });
+
+    const deleteSpecificData = (item) => {
+      const indexToDelete = newArray.indexOf(item);
+      if (indexToDelete !== -1) {
+        newArray.splice(indexToDelete, 1);
+        finalArray.splice(indexToDelete, 1);
+      }
+    };
 
     // Create inner container for label and delete icon
     const innerDiv = document.createElement("div");
@@ -74,7 +150,6 @@ const createForm = () => {
       outerDiv.remove();
       deleteSpecificData(item);
     });
-    console.log(newArray);
 
     // Append label and delete icon to inner container
     innerDiv.appendChild(label);
@@ -139,7 +214,7 @@ const deleteOption = (option, item) => {
 
 const saveBtn = document.getElementById("saveBtn");
 saveBtn.addEventListener("click", () => {
-  console.log("Save button clicked", newArray);
+  console.log(JSON.stringify(newArray));
 });
 
 // Call the createForm function to generate the form
